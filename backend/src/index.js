e6f91b83d8ea5b8bd52276224b6fded24861c7ca
Marcelo Ferreira <mah.tf@hotmail.com>
2020-01-15 11:53:49 -0300

    Finalização do Front End pra web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -9,8 +10,12 @@ const app = express();
 mongoose.connect('mongodb+srv://omnistack10:omnistack10@omnistack10-ndzuo.gcp.mongodb.net/week10?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: true,
 });
 
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(express.json());
 app.use(routes);
 
